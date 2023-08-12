6767e8123c5093a3e2adad5ddfa630030d1af522
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2023-08-11 18:39:11 -0600

    carousell

--- components/trendingFilms.js
+++ components/trendingFilms.js
@@ -1,10 +1,13 @@
+// import { styles } from '../theme';
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export default function trendingFilms() {
+
+
+export default function TrendingFilms() {
     return (
         <View className="mb-8">
-            <Text>Trending Films</Text>
+            <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
         </View>
     )
 }
