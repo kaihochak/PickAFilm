e24bca377bb20a6455d4d5a7a6212098ab150736
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2023-08-11 18:14:56 -0600

    homeScreen Top  Bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { styles } from '../theme';
+
+const ios = Platform.OS === 'ios';
 
 export default function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <Text className="text-red-500 text-4xl">RT</Text>
-            <StatusBar style="auto" />
+        <View className="flex-1 bg-neutral-800" >
+            {/* search bar and logo */}
+            <SafeAreaView className={ios? "-mb-2": 'mb-3'}>
+                <StatusBar style="light" />
+                <View className="flex-row justify-between items-center mx-4">
+
+                    {/* Menu */}
+                    <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+
+                    {/* Title */}
+                    <Text 
+                        className="text-white text-3xl font-bold">
+                        <Text style={styles.text}>Pick</Text>AFilm
+                    </Text>
+
+                    {/* Search */}
+                    <TouchableOpacity>
+                        <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+                    </TouchableOpacity>
+
+                </View>
+            </SafeAreaView>
+
+            {/* Film List */}
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 100}}
+            >
+                {/* Trending Films Carousel */}
+            </ScrollView>
         </View>
     )
 }
